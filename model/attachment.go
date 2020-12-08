65bad975d83769881d151d01eab0cf8a300c978a
fesion <tpyzlxy@163.com>
2020-12-08 22:16:21 +0800

    修复字段错误

--- model/attachment.go
+++ model/attachment.go
@@ -18,7 +18,7 @@ type Attachment struct {
 	Height       uint   `json:"height" gorm:"column:height;type:int(10) unsigned not null;default:0"`
 	Status       uint   `json:"status" gorm:"column:status;type:tinyint(1) unsigned not null;default:0;index:idx_status"`
 	CreatedTime  int64    `json:"created_time" gorm:"column:created_time;type:int(11) not null;default:0;index:idx_created_time"`
-	UpdatedTime  int64    `json:"created_time" gorm:"column:created_time;type:int(11) not null;default:0;index:idx_updated_time"`
+	UpdatedTime  int64    `json:"updated_time" gorm:"column:updated_time;type:int(11) not null;default:0;index:idx_updated_time"`
 	DeletedTime  int64    `json:"-" gorm:"column:deleted_time;type:int(11) not null;default:0"`
 	Logo         string `json:"logo" gorm:"-"`
 	Thumb        string `json:"thumb" gorm:"-"`
--- model/category.go
+++ model/category.go
@@ -14,7 +14,7 @@ type Category struct {
 	ParentId    uint    `json:"parent_id" gorm:"column:parent_id;type:int(10) unsigned not null;default:0;index:idx_parent_id"`
 	Status      uint   `json:"status" gorm:"column:status;type:tinyint(1) unsigned not null;default:0;index:idx_status"`
 	CreatedTime int64   `json:"created_time" gorm:"column:created_time;type:int(11) not null;default:0;index:idx_created_time"`
-	UpdatedTime int64   `json:"created_time" gorm:"column:created_time;type:int(11) not null;default:0;index:idx_updated_time"`
+	UpdatedTime int64   `json:"updated_time" gorm:"column:updated_time;type:int(11) not null;default:0;index:idx_updated_time"`
 	DeletedTime int64   `json:"-" gorm:"column:deleted_time;type:int(11) not null;default:0"`
 }
 
