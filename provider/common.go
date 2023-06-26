f843da96d58d1318d800ece1799baed3624a63da
Sinclair <tpyzlxy@gmail.com>
2023-06-26 09:08:22 +0800

    remove none use code

--- provider/common.go
+++ provider/common.go
@@ -6,32 +6,6 @@ import (
 	"log"
 )
 
-const (
-	IndexCacheKey = "index"
-
-	UserAgentPc     = "pc"
-	UserAgentMobile = "mobile"
-)
-
-func (w *Website) CacheIndex(ua string, body []byte) {
-	w.MemCache.Set(IndexCacheKey+ua, body, 3600)
-}
-
-func (w *Website) GetIndexCache(ua string) []byte {
-	body := w.MemCache.Get(IndexCacheKey + ua)
-
-	if body == nil {
-		return nil
-	}
-
-	content, ok := body.([]byte)
-	if ok {
-		return content
-	}
-
-	return nil
-}
-
 func (w *Website) DeleteCacheIndex() {
 	w.RemoveHtmlCache("/")
 }
