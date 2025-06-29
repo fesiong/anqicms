ef682d1d8eae0fc5a9b3400bfe44d6ced2cbfbe0
Sinclair <tpyzlxy@gmail.com>
2025-06-29 10:04:02 +0800

    ContentTitles 支持 扁平模式和children模式，showType=tree|children

--- tags/archiveDetail.go
+++ tags/archiveDetail.go
@@ -204,7 +204,14 @@ func (node *tagArchiveDetailNode) Execute(ctx *pongo2.ExecutionContext, writer p
 				}
 				if fieldName == "ContentTitles" {
 					ctx.Private["showContentTitle"] = true
-					content, _ = library.ParseContentTitles(tmpContent)
+					showType := "tree" // 只支持 tree，children
+					if args["showType"] != nil {
+						showType = args["showType"].String()
+						if showType != "tree" && showType != "children" {
+							showType = "tree"
+						}
+					}
+					content, _ = library.ParseContentTitles(tmpContent, showType)
 				} else {
 					//添加锚文本, 2 = 渲染的时候替换
 					if currentSite.PluginAnchor.ReplaceWay == 2 {
@@ -292,7 +299,7 @@ func (node *tagArchiveDetailNode) Execute(ctx *pongo2.ExecutionContext, writer p
 					}
 					tmpContent = currentSite.ReplaceContentUrl(tmpContent, true)
 					if isShow, ok := ctx.Private["showContentTitle"]; ok && isShow == true {
-						_, tmpContent = library.ParseContentTitles(tmpContent)
+						_, tmpContent = library.ParseContentTitles(tmpContent, "tree")
 					}
 					content = tmpContent
 				}
