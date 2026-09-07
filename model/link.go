43ab76ada67ebd0759fcb6b55b44abd13b897599
Sinclair <tpyzlxy@gmail.com>
2026-09-07 11:35:14 +0800

    fix logo field of friendlink

--- model/link.go
+++ model/link.go
@@ -28,7 +28,7 @@ type Link struct {
 	Sort        uint   `json:"sort" gorm:"column:sort;type:int(10) unsigned not null;default:99;index:idx_sort"`
 	Status      uint   `json:"status" gorm:"column:status;type:tinyint(1) unsigned not null;default:0;index:idx_status"`
 	CheckedTime int64  `json:"checked_time" gorm:"column:checked_time;type:bigint(20) not null;default:0"`
-	Logo        string `json:"logo" gorm:"column:title;type:varchar(logo) not null;default:''"`
+	Logo        string `json:"logo" gorm:"column:logo;type:varchar(250) not null;default:''"`
 	Thumb       string `json:"thumb" gorm:"-"`
 }
 
